6e25eb63c578136dba3345bd4a73bb006608fedd
dmontenegroh <dmontenegro.top@gmail.com>
2025-06-13 22:35:19 -0400

    Prueba de CI

--- sum.js
+++ sum.js
@@ -3,4 +3,13 @@ function sum(a, b) {
   return a + b;
 }
 
+function sumAsync(a, b) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(a + b);
+    }, 1000);
+  });
+}
+
 module.exports = sum;
+module.exports.sumAsync = sumAsync;
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,12 @@
 const sum = require('./sum');
+const sumAsync = require('./sum').sumAsync;
 
 test('Suma dos números', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('Suma dos números asíncrona', async () => {
+  const result = await sumAsync(1, 2);
+  expect(result).toBe(3);
+});
+
